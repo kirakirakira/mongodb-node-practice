688aa22334ac361dc766db3cfba47fe4b9160758
Kira Awesome <kembrekl@gmail.com>
2016-01-13 18:51:15 +0000

    adding templating

--- Week 1 Challenge/movie_app.js
+++ Week 1 Challenge/movie_app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app = express(),
     engines = require('consolidate'),
-    bodyParser = require('body-parser')
+    bodyParser = require('body-parser'),
+    assert = require('assert');
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
@@ -29,8 +30,68 @@ function errorHandler(err, req, res, next) {
     });
 }
 
-app.get('/', function(req, res, next) {
-    res.render('movie');
+app.get('/', function(req, res) {
+    
+    db.collection('movie_collection').find({}).toArray(function(err, docs) {
+        res.render('movie', {'movie': docs});
+    });
+
+    // res.render('movie');
+    // var moviesString = [];
+    // var movie_title = [];
+    // var movie_year = [];
+    // var movie_imdb = [];
+
+    // var findMovies = function(db, callback) {
+    //     var cursor = db.collection('movie_collection').find();
+    //     cursor.each(function(err, doc) {
+    //         assert.equal(err, null);
+    //         if (doc != null) {
+    //             movie_title += doc.title;
+    //             movie_year += doc.year;
+    //             movie_imdb += doc.imdb;
+    //         }
+    //         else {
+                
+    //             res.render('movie', {title: movie_title, year: movie_year, imdb: movie_imdb});
+    // //             res.send('<html>\
+    // //     <body>\
+    // //         <h1>Movie List Maker!</h1>\
+    // //         <form action="/movies" method="POST">\
+    // //         Title: <input type="text" name="title" value="title"><br>\
+    // //         Year: <input type="text" name="year" value="year"><br>\
+    // //         IMDB: <input type="text" name="imdb" value="imdb"><br>\
+    // //         <input type="submit" value="Submit">\
+    // //         </form>\
+    // //         <h2>Movie List</h2>\
+    // //         <div id="movie_list">' + '<ul><li>' + movie_title + '</li>' + 
+    // //         '<li>' + movie_year + '</li>' + 
+    // //         '<li>' + movie_imdb + '</li>' +
+    // //                 '</div>\
+    // //         <script src="movie_app.js"></script>\
+    // //     </body>\
+    // // </html>');
+    //         }
+    //     });
+    // };
+
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        findMovies(db, function() {
+            db.close();
+        });
+    });
+
+    if (moviesString.length === 0) {
+        // for (movie of movies) {
+        //     moviesString += 'movie stuff';
+        // }
+        moviesString = '<p id="no_movies">No movies added yet.</p>'
+    }
+    // else {
+    //     moviesString = '<p id="no_movies">No movies added yet.</p>'
+    // }
+
 });
 
 app.post('/movies', function(req, res, next) {
@@ -42,7 +103,8 @@ app.post('/movies', function(req, res, next) {
         next('Please fill in all fields.');
     }
     else {
-        res.send('Form is completed. You entered: ' + title + ' ' + year + ' ' + imdb + '.');
+        //res.send('Form is completed. You entered: ' + title + ' ' + year + ' ' + imdb + '.');
+        res.redirect('..');
     }
 
     // Use connect method to connect to the Server
